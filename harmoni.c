3c233a70a248abb77eefd746b856386a25bb535e
Tom Soulanille <soul@prama.com>
2013-12-05 22:47:25 -0800

    quickie harmonic series in c

---
+++ harmoni.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    double n, sum;
+    int i, j;
+
+    // Harmonic series is a fine way to burn CPU
+    sum = n = 0.0;
+    j = 0;
+    while(1){
+        for (i=0; i<10000000; i++) {
+            n += 1.0;
+            sum += 1.0 / n;
+        }
+        j++;
+        printf("Sum 1/n for in in [1..%d000001] = %f\r\n", j, sum);
+    }
+}
